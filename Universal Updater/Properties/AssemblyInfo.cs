bdabbafdc1e7a16a3d213af596014a1411b32e24
Bashar Astifan <accounts@astifan.online>
2024-10-10 10:39:28 +0400

    Suggested improvements and fixes

--- Universal Updater/Properties/AssemblyInfo.cs
+++ Universal Updater/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fadil Fadz")]
 [assembly: AssemblyProduct("Universal Updater")]
-[assembly: AssemblyCopyright("Copyright (c) 2021 - 2023")]
+[assembly: AssemblyCopyright("Copyright (c) 2021 - 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.6.0.0")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
